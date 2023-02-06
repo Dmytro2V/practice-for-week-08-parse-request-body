acecce44e985541eafddf2c79362da3a4faf00d9
Dmytro2V <op2vin@gmail.com>
2023-02-06 21:00:39 +0200

    working

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const { sendFormPage } = require("./routes");
 const { parseBody } = require("./parse-body");
 let server;
-
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
-
+const http = require("http");
+server = http.createServer((req,res) => {
+    console.log(req);
+    let method='';
+    console.log(url);
+    let reqBody ='';
+    req.on('method', (meth) => method = meth);
+    //req.on('url', (ur) => url = ur);
+    req.on('data', (dat) => reqBody += dat)
+    req.on('end', () => {
+        console.log('method = ', method);
+        console.log('URL = ', url);
+        console.log('data all = ', reqBody);
+    });
+    res.end();
+});
+const port = 5000;
+server.listen(port, console.log('Server is listening on port', port));
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
 
